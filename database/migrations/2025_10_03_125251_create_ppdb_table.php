42ffeba397a9a2a3e65ee2ee7351b84d83270971
superevilstockholm <balilombok11@gmail.com>
2025-10-03 20:28:02 +0700

    Created database migration for table ppdb

---
+++ database/migrations/2025_10_03_125251_create_ppdb_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ppdb', function (Blueprint $table) {
+            $table->id();
+            $table->enum('jenis_pendaftaran', ['Peserta Didik Baru', 'Mutasi']);
+            $table->enum('jenjang', ['TK', 'SD', 'SMP', 'SMA']);
+            $table->string('nama_lengkap');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('no_telp')->nullable();
+            $table->string('no_hp')->nullable();
+            $table->string('no_hp2')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ppdb');
+    }
+};
