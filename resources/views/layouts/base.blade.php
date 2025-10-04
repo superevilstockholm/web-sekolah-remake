1568e61f0eefa72dd5d79d004525cd4400fc7dac
superevilstockholm <balilombok11@gmail.com>
2025-10-04 23:43:11 +0700

    Created base layout and created login page (WIP)

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    {{-- SEO Tags --}}
+    <meta name="description" content="@yield('description', 'Web Sekolah, Al-Layyinah')">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Al-Layyinah')</title>
+    <link rel="stylesheet" href="{{ asset('static/bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <noscript>Please enable JavaScript to continue using this application.</noscript>
+    <script>function getAuthToken() { const token = document.cookie.replace(/(?:(?:^|.*;\s*)auth_token\s*\=\s*([^;]*).*$)|^.*$/, '$1'); return token ? decodeURIComponent(token) : null; }</script>
+    @if ($meta['showNavbar'] ?? true)
+        <x-navbar></x-navbar>
+    @endif
+    <main>
+        @yield('content')
+    </main>
+    @if ($meta['showFooter'] ?? true)
+        <x-footer></x-footer>
+    @endif
+    {{-- Axios --}}
+    <script src="{{ asset('static/js/axios.min.js') }}"></script>
+    {{-- Sweetalert2 --}}
+    <script src="{{ asset('static/js/sweetalert2@11.js') }}"></script>
+    {{-- Bootstrap --}}
+    <script src="{{ asset('static/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.index');
+})->name('index');
+
+// Auth
+Route::get('login', function () {
+    return view('pages.auth.login', [
+        'meta' => [
+            'showNavbar' => false,
+            'showFooter' => false
+        ]
+    ]);
+})->name('login');
 
 Route::middleware(['auth.sanctum.cookie'])->group(function () {
+    // System
     Route::get('status', function () {
         return true;
-    });
+    })->name('status');
+
+    // Dashboard
+    Route::get('dashboard', function () {
+        return view('pages.protected.index');
+    })->name('dashboard.index');
 });
